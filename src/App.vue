4d1fcc39040adc6447e32d1e04dd1f280c330714
redamoon <yuuki60824@gmail.com>
2024-06-09 20:17:58 +0900

    v-commbox

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,73 @@
 <template>
   <v-app id="app">
     <v-container>
-      <v-btn color="primary" large>Vuetify 2 Button</v-btn>
+      <v-combobox
+        label="ユーザの選択"
+        :items="items"
+        :value="value"
+        item-text="name"
+      >
+        <template #prepend-item>
+          <v-list-item @click="clickPrepend">
+            <v-list-item-content>
+              <v-list-item-title> モーダルを表示 </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider class="mt-2 mb-1" />
+        </template>
+        <template #selection="{ item }"> {{ item.name }}</template>
+        <template #item="{ item }">
+          <v-list-item-title>{{ item.name }}</v-list-item-title>
+        </template>
+      </v-combobox>
+
+      <v-dialog v-model="dialog" width="500">
+        <v-card>
+          <v-card-title class="text-h5 grey lighten-2">
+            モーダルダイアログ
+          </v-card-title>
+
+          <v-card-text> モーダルダイアログの内容 </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" text @click="dialog = false">
+              I accept
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-container>
   </v-app>
 </template>
 
+<script>
+export default {
+  data: () => ({
+    dialog: false,
+    items: [
+      { name: "Ken", url: "https://randomuser.me/apix" },
+      { name: "John", url: "https://randomuser.me/apix" },
+    ],
+    value: "",
+  }),
+  methods: {
+    clickPrepend() {
+      this.dialog = true;
+      console.log("clickPrepend");
+      this.$emit("click:prepend");
+    },
+  },
+};
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
